a7bb6e0c76e3b1fea651cd9568daaebeeb8dde11
FewX <garfield8283@gmail.com>
2024-05-22 03:00:13 +0700

    edit port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 require("dotenv").config();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
